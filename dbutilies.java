a6f449a1613a9b0606b1b483d49a9a3ffc386864
gondrala <gondralavenkatesh9@gmail.com>
2022-10-31 11:51:08 +0530

    update

--- dbutilies.java
+++ dbutilies.java
@@ -1,3 +1,5 @@
 this line is updated . .
 hello every one this file contian db related codie
 hai venkatesh anna how are you
+hii ramesh todat i updated this line because you give or not write permissions 
+to me
